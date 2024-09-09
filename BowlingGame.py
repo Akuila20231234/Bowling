5adab1676bfd8d126fb1f92b27cf6418b467a10f
Akuila20231234 <143366513+Akuila20231234@users.noreply.github.com>
2024-09-09 20:39:37 +1200

    Docstring
    Docstrings used to explain what is going on with each line of code. Listing the Arguments as well as the parameters

--- BowlingGame.py
+++ BowlingGame.py
@@ -8,11 +8,28 @@ class BowlingGame:
     def score(self):
         total_score = 0
         roll_index = 0
+        """
+        __init__ method:
+        Parameter: self
+        This initializes an instance of the BowlingGame class and creates an empty list called rolls to store the pins knocked down in each roll.
 
+        roll method:
+        Parameters: self, pins
+        Argument (example): pins (e.g., when calling game.roll(5))
+        This method records the result of a roll by appending the number of pins knocked down (passed as the argument pins) to the rolls list.
+
+        score method:
+        Parameter: self
+        The score method is intended to calculate the total score of the game. The variables total_score and roll_index are initialized to keep track of the total score and the current roll being processed, respectively. The for loop is likely intended to loop through the 10 frames of a bowling game, though the loop itself is incomplete in the provided code.
+        """
+        
         for _ in range(10):
             if self.is_strike(roll_index):
                 total_score += self.strike_score(roll_index)
                 roll_index += 1
+                """
+
+                """
             elif self.is_spare(roll_index):
                 total_score += self.spare_score(roll_index)
                 roll_index += 2

fc1563492fdb3f982277b122e3d99e6732e16b76
Akuila20231234 <143366513+Akuila20231234@users.noreply.github.com>
2024-09-09 20:57:17 +1200

    DocStrings
    More docstrings that explains what each line of code does, while listing the arguments as well as the parameters.

--- BowlingGame.py
+++ BowlingGame.py
@@ -28,14 +28,47 @@ class BowlingGame:
                 total_score += self.strike_score(roll_index)
                 roll_index += 1
                 """
+                Arguments: 
+                is_strike
+                strike_score
 
+                Parameter: 
+                if self.is_strike(roll_index):
+                This condition checks if the current roll, referenced by roll_index, is a strike (when all 10 pins are knocked down on the first roll of the frame). If it is, the code proceeds with the strike logic.
+                
+                total_score += self.strike_score(roll_index):
+                If a strike is detected, the strike_score method is called to calculate the score for the strike (usually 10 plus the sum of the next two rolls), and this score is added to total_score.
+                
+                roll_index += 1
+                After processing a strike, roll_index is incremented by 1 to move to the next roll. In the case of a strike, only one roll is counted for the frame.
                 """
             elif self.is_spare(roll_index):
                 total_score += self.spare_score(roll_index)
                 roll_index += 2
+
+                """
+                elif self.is_spare(roll_index):
+                This condition checks if the current frame, starting at roll_index, is a spare. A spare occurs when all 10 pins are knocked down in two rolls within the frame.
+               
+                total_score += self.spare_score(roll_index):
+                If a spare is detected, the spare_score method is called to calculate the score for the spare. In bowling, the score for a spare is 10 plus the number of pins knocked down in the next roll. The result is added to total_score.
+                
+                roll_index += 2:
+                After processing a spare, roll_index is incremented by 2 to move to the next frame, as a spare consumes two rolls within the current frame.
+                """
             else:
                 total_score += self.frame_score(roll_index)
                 roll_index += 2
+                """
+                else:
+                This handles the case where the current frame is neither a strike nor a spare. It processes a normal frame where the player needed two rolls to knock down all the pins (or failed to knock them all down).
+                
+                total_score += self.frame_score(roll_index):
+                The frame_score method is called to calculate the total pins knocked down in the current frame (two rolls). The sum of the pins from both rolls in the frame is added to total_score.
+                
+                roll_index += 2:
+                Since a regular frame consists of two rolls, roll_index is incremented by 2 to move to the next frame.
+                """
 
         return total_score
 
@@ -53,3 +86,20 @@ class BowlingGame:
 
     def frame_score(self, roll_index):
         return self.rolls[roll_index] + self.rolls[roll_index + 1]
+    
+    """
+    is_strike(self, roll_index)
+    Checks if the roll at roll_index is a strike (i.e., if it knocked down all 10 pins). Returns True if it is a strike, otherwise False.
+    
+    is_spare(self, roll_index)
+    Checks if the sum of the pins knocked down in the roll at roll_index and the next roll (at roll_index + 1) equals 10. Returns True if it’s a spare, otherwise False.
+    
+    strike_score(self, roll_index)
+    Calculates the score for a strike. A strike is worth 10 points plus the total pins knocked down in the next two rolls (at roll_index + 1 and roll_index + 2).
+    
+    spare_score(self, roll_index)
+    Calculates the score for a spare. A spare is worth 10 points plus the total pins knocked down in the roll immediately following the spare (at roll_index + 2).
+    
+    frame_score(self, roll_index)
+    Calculates the score for a regular frame. This method sums up the pins knocked down in the roll at roll_index and the next roll (at roll_index + 1).
+    """
